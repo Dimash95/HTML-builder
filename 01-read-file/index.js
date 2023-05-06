76e79b5096e1730b11535889e71edd102239383c
Dimash <dinmukhamed.amirov@gmail.com>
2023-05-06 11:30:59 +0600

    refactor: use ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = require('process');
+const input = fs.ReadStream(path.join('01-read-file', 'text.txt'));
+input.on('data', data => stdout.write(data));
 
-fs.readFile(
-  path.join('01-read-file', 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+
+
