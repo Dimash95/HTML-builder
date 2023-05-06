2be2ed776af6ba651e8b3f89bb851a916a3a7680
Dimash <dinmukhamed.amirov@gmail.com>
2023-05-06 09:21:37 +0600

    feat: change 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const {stdout} = process;
-const stream = fs.createReadStream(path.join('01-read-file', '/text.txt'), 'utf-8');
 
-let data = '';
-
-stream.on('data', chunk => stdout.write(data += chunk));
+fs.readFile(
+  path.join('01-read-file', 'text.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
