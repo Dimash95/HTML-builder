8c80d67657278f70329c51078d76b6872a4c5f34
Dimash <dinmukhamed.amirov@gmail.com>
2023-05-08 15:56:25 +0600

    copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+let path = require('path');
+let fs = require('fs');
+let dir = path.join(__dirname, 'files');
+let dircopy = path.join(__dirname, 'files-copy');
 
+
+fs.stat(dircopy, function (err) {
+  if (!err) {
+    console.log('Директория есть');
+    fs.readdir(dircopy, (err, files) => {
+      if (err) throw err;
+      for (let file of files) {
+        fs.unlink(dircopy + '/' + file, (err) => {
+          if (err) throw err;
+        }
+        )
+      }
+      fs.readdir(dir, (err, files) => {
+        if (err) throw err;
+        for (let file of files) {
+          fs.copyFile(dir + '/' + file, dircopy + '/' + file, (err) => {
+            if (err) throw err;
+          })
+        }
+      });
+    })
+  } else if (err.code === 'ENOENT') {
+    console.log('директории нет');
+    fs.mkdir(dircopy, err => {
+      if (err) throw err;
+      fs.readdir(dir, (err, files) => {
+        if (err) throw err;
+        for (let file of files) {
+          fs.copyFile(dir + '/' + file, dircopy + '/' + file, (err) => {
+            if (err) throw err;
+          })
+        }
+
+      });
+    });
+  }
+});
